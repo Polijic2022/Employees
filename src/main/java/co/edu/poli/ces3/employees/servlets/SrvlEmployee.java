7c154130dab1bf1e314e3f4f4a5966e74ef95e50
MJRV91 <miltonjrv@gmail.com>
2022-06-30 21:01:43 -0500

    Creación de tabla, formulario y Arrays

---
+++ src/main/java/co/edu/poli/ces3/employees/servlets/SrvlEmployee.java
@@ -0,0 +1,51 @@
+package co.edu.poli.ces3.employees.servlets;
+
+import co.edu.poli.ces3.employees.entities.Employee;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet(name = "SrvlEmployee", value = "/SrvlEmployee")
+public class SrvlEmployee extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletOutputStream out = response.getOutputStream();
+        response.setContentType("application/json");
+        //out.print("número 1: " + request.getParameter("numero1"));
+        //out.print("número 2: " + request.getParameter("numero2"));
+        int n1 = Integer.parseInt(request.getParameter("numero1"));
+        int n2 = Integer.parseInt(request.getParameter("numero2"));
+
+        //out.print("La suma es: " + (n1 + n2));
+        //out.print("<br/><br/><br/><br/>el token es: "+ request.getHeader("TOKEN"));
+        out.print("{nombre: 'milton'}");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //ServletOutputStream out = response.getOutputStream();
+        ArrayList<Employee> employees = new ArrayList<>();
+
+        /*response.setContentType("text/html");
+        out.print("<p>Nombre: "+request.getParameter("name")+"</p>");
+        out.print("<p>Edad: "+request.getParameter("age")+"</p>");*/
+        Employee employee = new Employee(
+                        request.getParameter("id"),
+                        request.getParameter("name"),
+                        request.getParameter("lastName"),
+                        Integer.parseInt(request.getParameter("age"))
+
+                );
+
+                employees.add(employee);
+
+                request.setAttribute("employees", employees);
+
+                request.getRequestDispatcher("/views/employees/list.jsp").forward(request,response);
+
+    }
+}
---
+++ src/main/webapp/views/employees/list.jsp
@@ -0,0 +1,43 @@
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="co.edu.poli.ces3.employees.entities.Employee" %><%--
+  Created by IntelliJ IDEA.
+  User: mrestrepov
+  Date: 30/06/2022
+  Time: 4:43 p. m.
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%
+    ArrayList<Employee> listEmployees =(ArrayList<Employee>) request.getAttribute("employees");
+%>
+<html>
+<head>
+    <title>Lista de empleados</title>
+</head>
+<body>
+<table id="tbl_employees">
+    <thead >
+    <tr>
+        <th>Identificación</th>
+        <th>Nombre</th>
+        <th>Apellido</th>
+        <th>Edad</th>
+    </tr>
+    </thead>
+    <tbody>
+    <%
+        for (Employee x: listEmployees) {
+    %>
+    <tr>
+        <td><%= x.getId() %></td>
+        <td><%=x.getName() %></td>
+        <td><%= x.getLastName() %></td>
+        <td><%= x.getAge() %></td>
+    </tr>
+    <%
+        }
+    %>
+    </tbody>
+</table>
+</body>
+</html>
